37b14800c77f7adac67d7b424c797af7db9f0830
Yaswanth__7 <yaswanthamjuri@gmail.com>
2025-09-12 23:02:14 +0530

    java updated

---
+++ java/patterns/p4.java
@@ -0,0 +1,29 @@
+public class p4 {
+    public static void main(String[] args) {
+        int n = 5;
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=0;j<n-i-1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=0;k<2*i+1;k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++){
+                if(i==n-1 || k==0 || k==2*i){
+                System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
