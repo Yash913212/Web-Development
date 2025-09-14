46e4197ed6596a20320922cde63ce37b8afbeb4e
Yaswanth__7 <yaswanthamjuri@gmail.com>
2025-09-14 21:28:02 +0530

    Java Patters

--- java/patterns/Pattern4.java
+++ java/patterns/Pattern4.java
@@ -1,4 +1,4 @@
-public class p4 {
+public class Pattern4 {
     public static void main(String[] args) {
         int n = 5;
         // for(int i=n-1;i>=0;i--){
@@ -10,7 +10,8 @@ public class p4 {
         //     }
         //     System.out.println();
         // }
-        for(int i=0;i<n;i++){
+        // for(int i=0;i<n;i++){
+        for(int i=n-1;i>=0;i--){
             for(int j=0;j<n-i-1;j++){
                 System.out.print(" ");
             }
--- java/patterns/Pattern5.java
+++ java/patterns/Pattern5.java
@@ -1,4 +1,4 @@
-public class p5 {
+public class Pattern5 {
     public static void main(String[] args) {
         // int n = 5;
         // for(int i=0;i<n;i++){
